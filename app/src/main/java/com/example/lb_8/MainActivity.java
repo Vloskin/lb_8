77a4ea991be6b10e5aa781aadd3f27a41df584cb
Vlosk <Vloskin@outlook.com>
2024-01-03 16:55:44 +0300

    Luka

--- app/src/main/java/com/example/lb_8/MainActivity.java
+++ app/src/main/java/com/example/lb_8/MainActivity.java
@@ -6,17 +6,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button button1;
-    Button button2;
-    Button button3;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,111 +22,98 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
+        TextView textView = findViewById(R.id.ContextMenu);
+        registerForContextMenu(textView);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openOptionsMenu();
-            }
-        });
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openContextMenu(v);
-            }
-        });
+        Button button = findViewById(R.id.popupMenuButton);
+        button.setOnClickListener(viewClickListener);
 
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showPopupMenu(v);
-            }
-        });
 
-        registerForContextMenu(button2);
     }
-
+// Create Options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        menu.add("menu1");
-        menu.add("menu2");
-        menu.add("menu3");
-        menu.add("menu4");
+        getMenuInflater().inflate(R.menu.options_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(item.getItemId() == R.id.item_settings1)
-        {
-            Toast.makeText(this, "Пункт 1", Toast.LENGTH_SHORT).show();
-            return true;
+
+        if(item.getItemId() == R.id.menu_item1 ) {
+            Toast.makeText(this, "Выбран пункт 1", Toast.LENGTH_SHORT).show();
         }
-        else if (item.getItemId() == R.id.item_settings2)
-        {
-            Toast.makeText(this, "Пункт 2", Toast.LENGTH_SHORT).show();
-            return true;
+        else if (item.getItemId() == R.id.menu_item2) {
+            Toast.makeText(this, "Выбран пункт 2", Toast.LENGTH_SHORT).show();
         }
-        else
-        {
-            return super.onOptionsItemSelected(item);
+        else if (item.getItemId() == R.id.menu_item3) {
+            Toast.makeText(this, "Выбран пункт 3", Toast.LENGTH_SHORT).show();
         }
+
+        return super.onOptionsItemSelected(item);
+
     }
 
+
+    //create Context menu
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.context_menu, menu);
+
+        menu.add(0, 0, 0, "Пункт 1");
+        menu.add(0, 1, 1, "Пункт 2");
     }
+
+
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.item_context1)
-        {
-            Toast.makeText(this, "Пункт 1", Toast.LENGTH_SHORT).show();
-            return true;
+
+
+        if(item.getItemId() == 0 ) {
+            Toast.makeText(this, "Выбран пункт 1", Toast.LENGTH_SHORT).show();
         }
-        else if (item.getItemId() == R.id.item_context2)
-        {
-            Toast.makeText(this, "Пункт 2", Toast.LENGTH_SHORT).show();
-            return true;
+        else if (item.getItemId() == 1) {
+            Toast.makeText(this, "Выбран пункт 2", Toast.LENGTH_SHORT).show();
         }
-        else
-        {
-            return super.onContextItemSelected(item);
-        }
+
+        return super.onOptionsItemSelected(item);
+
     }
+
+//Create popup menu
+View.OnClickListener viewClickListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        showPopupMenu(v);
+    }
+};
 
     private void showPopupMenu(View v) {
         PopupMenu popupMenu = new PopupMenu(this, v);
-        popupMenu.inflate(R.menu.menu_popup);
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
+        popupMenu.inflate(R.menu.popup_menu);
 
-                if(item.getItemId() == R.id.item_context1)
-                {
-                    Toast.makeText(MainActivity.this, "Пункт 1", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                else if (item.getItemId() == R.id.item_context2)
-                {
-                    Toast.makeText(MainActivity.this, "Пункт 2", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+        popupMenu
+                .setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
 
-            }
-        });
+
+                        if(item.getItemId() == R.id.menu1 ) {
+                            Toast.makeText(getApplicationContext(), "Выбран пункт 1", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (item.getItemId() == R.id.menu2) {
+                            Toast.makeText(getApplicationContext(), "Выбран пункт 2", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (item.getItemId() == R.id.menu3) {
+                            Toast.makeText(getApplicationContext(), "Выбран пункт 3", Toast.LENGTH_SHORT).show();
+                        }
+                                return false;
+                    }
+                });
         popupMenu.show();
     }
 }
